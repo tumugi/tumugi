e73e38411cb6ff823f6f2562437e42fc248f5afc
Kazuyuki Honda <hakobera@gmail.com>
2016-04-10 19:51:38 +0900

    Fix test

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -6,10 +6,10 @@ class Tumugi::HelperTest < Test::Unit::TestCase
   data(
     'nil'    => [[], nil],
     'array'  => [[1,2], [1,2]],
-    'hash'   => [{key1: 'val1', key2: 'val2'}, ['val1', 'val2']],
+    'hash'   => [['val1', 'val2'], {key1: 'val1', key2: 'val2'}],
     'object' => [['test'], 'test'],
   )
-  test '#list' do |expected, obj|
+  test '#list' do |(expected, obj)|
     assert_equal(expected, list(obj))
   end
 end
